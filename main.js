9dce67917ddce111ea751ecd9e310245cf1e302d
musabapoisha <musabapoisha@gmail.com>
2023-02-04 12:37:31 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let menu = document.querySelector("header nav .links .menu");
-let ul = document.querySelector("header nav .links ul");
-
-menu.addEventListener("click", function (e) {
-  ul.classList.toggle("hidden");
-});
-console.log(menu);
